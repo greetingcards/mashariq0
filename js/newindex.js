dc174aa1c1a557fb0730a1acfba0acdcbc149a40
greetingcards <162377092+greetingcards@users.noreply.github.com>
2024-03-07 04:09:37 +0300

    Add files via upload

--- js/newindex.js
+++ js/newindex.js
@@ -8,63 +8,7 @@ img.crossOrigin="anonymous";
 window.addEventListener('load', DrawPlaceholder(img,ctx,"#7E9E85",'111.jpg',320,240));
 
 
-var canvas1 = document.getElementById('imageCanvas1');
-var ctx1 = canvas1.getContext('2d');
-var img1 = new Image();
-img1.crossOrigin="anonymous";
 
-window.addEventListener('load', DrawPlaceholder(img1,ctx1,"#D3CFC8",'222.jpg',225,330));
-
-
-var canvas2 = document.getElementById('imageCanvas2');
-var ctx2 = canvas2.getContext('2d');
-var img2 = new Image();
-img2.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img2,ctx2,"#EeEdDf",'333.jpg',110,380));
-
-
-var canvas3 = document.getElementById('imageCanvas3');
-var ctx3 = canvas3.getContext('2d');
-var img3 = new Image();
-img3.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img3,ctx3,"#A89172",'444.jpg',215,250));
-
-
-
-var canvas4 = document.getElementById('imageCanvas4');
-var ctx4 = canvas4.getContext('2d');
-var img4 = new Image();
-img4.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img4,ctx4,"#7E9E85",'555.jpg',320,220));
-
-
-
-var canvas5 = document.getElementById('imageCanvas5');
-var ctx5 = canvas5.getContext('2d');
-var img5 = new Image();
-img5.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img5,ctx5,"#D3CFC8",'666.jpg',225,350));
-
-
-
-var canvas6 = document.getElementById('imageCanvas6');
-var ctx6 = canvas6.getContext('2d');
-var img6 = new Image();
-img6.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img6,ctx6,"#EeEdDf",'777.jpg',110,380));
-
-
-var canvas7 = document.getElementById('imageCanvas7');
-var ctx7 = canvas7.getContext('2d');
-var img7 = new Image();
-img7.crossOrigin="anonymous";
-
-window.addEventListener('load', DrawPlaceholder(img7,ctx7,"#A89172",'888.jpg',215,260));
 
 
 
@@ -107,33 +51,5 @@ download_img = function(el) {
   el.href = image;
 };
 
-download_img1 = function(el) {
-  var image1 = canvas1.toDataURL("image/jpg");
-  el.href = image1;
-};
 
-download_img2 = function(el) {
-  var image2 = canvas2.toDataURL("image/jpg");
-  el.href = image2;
-};
-
-download_img3 = function(el) {
-  var image3 = canvas3.toDataURL("image/jpg");
-  el.href = image3;
-};
-
-download_img4 = function(el) {
-  var image4 = canvas4.toDataURL("image/jpg");
-  el.href = image4;
-};
-
-download_img5 = function(el) {
-  var image5 = canvas5.toDataURL("image/jpg");
-  el.href = image5;
-};
-
-download_img6 = function(el) {
-  var image6 = canvas6.toDataURL("image/jpg");
-  el.href = image6;
-};
 
